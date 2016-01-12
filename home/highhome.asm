39f25501689975b338338cfbe77b94db8f0f0a18
ProjectRevoTPP <projectrevotpp@hotmail.com>
2016-01-11 22:34:19 -0500

    did I do thaaaaaat (LUA serial copies to military bytes)
    attempt at making LUA serial copy to DFF8 and DFF9 repsectively.. i may have broken something.. yell at me if I did

--- home/highhome.asm
+++ home/highhome.asm
@@ -11,6 +11,8 @@ ResetLUASerial::
 	
 _LUASerial:
 	; Send a to an external AI computer and store the response in a
+	ld b, 0
+.beginning
 	ld [hLSB], a
 	ld a, BEESAFREE_LSC_TRANSFERRING
 	ld [hLSC], a
@@ -19,5 +21,13 @@ _LUASerial:
 	cp BEESAFREE_LSC_COMPLETED
 	jr nz, .wait
 	ld a, [hLSB]
+	inc b
+	cp 2
+	jr z, .second
+	ld [wdff8], a
+	jr .beginning
+.second
+	ld a, [hLSB]
+	ld [wdff9], a
 	ret
 ENDC
