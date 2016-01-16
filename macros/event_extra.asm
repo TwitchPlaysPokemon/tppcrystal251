7db4aee0ebb008247d234ebb2d3a0a814db000eb
yenatch <yenatch@gmail.com>
2016-01-16 14:47:21 -0500

    trainer macro already exists in macros/trainer.asm

--- macros/event_extra.asm
+++ macros/event_extra.asm
@@ -6,10 +6,3 @@ givetm: macro
 	iffalse \4
 	setevent \2
 endm
-
-; trainer event_Flag, class, trainer, seen_text, beaten_text, ?, after_script
-trainer: macro
-	dw \1
-	db \2, \3
-	dw \4, \5, \6, \7
-endm
