ebe2e9b5c3e9b88fb096235ec22807112e97c58f
PikalaxALT <PikalaxALT@gmail.com>
2016-01-22 21:55:34 -0500

    Forgot to add an event flag to std script InitEvents

--- engine/std_scripts.asm
+++ engine/std_scripts.asm
@@ -903,6 +903,7 @@ InitializeEventsScript: ; 0xbc3db
 	setevent EVENT_ROCKETS_SS_ANNE_2
 	setevent EVENT_ROCKETS_SS_ANNE_1
 	setevent EVENT_SS_ANNE_SIMON
+	setevent EVENT_S_S_ANNE_7_ROCKETS
 	return
 ; 0xbc574
 
