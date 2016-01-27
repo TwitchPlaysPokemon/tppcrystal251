f6c2b9ea57cae074c3d4b73069712164e98e4650
Pigu-A <supa7764@gmail.com>
2016-01-27 11:12:18 +0700

    Remix GS intro music

--- audio/music/goldsilveropening.asm
+++ audio/music/goldsilveropening.asm
@@ -4,618 +4,568 @@ Music_GoldSilverOpening: ; f7055
 	dbw $02, Music_GoldSilverOpening_Ch3
 ; f705e
 
-Music_GoldSilverOpening_Ch1: ; f705e
-	tempo 256
-	volume $77
-	tone $0001
-	dutycycle $2
+Music_GoldSilverOpening_Ch1:
+	tempo $100
+	dutycycle $1
+	notetype $c, $14
+	vibrato $8, $12
 	stereopanning $f0
-	notetype $c, $b7
-	octave 1
-	note G#, 16
-	note G#, 16
-	tempo 168
-	dutycycle $3
-	notetype $6, $c7
-	octave 3
-	note F_, 12
-	note A_, 4
+; Pat 0 Row 0
+; Pat 1 Row 0
+; Pat 2 Row 0
+	callchannel Music_GoldSilverOpening_P1
+; Pat 3 Row 0
+	note G_, 6
+	note B_, 2
 	octave 4
-	note C_, 12
+	note D_, 4
+	octave 2
+	note G_, 4
 	octave 3
 	note B_, 2
-	note A#, 2
-	note G_, 12
-	note A#, 4
 	octave 4
-	note D#, 12
-	note D_, 2
-	note C#, 2
-	notetype $6, $b5
-	note C_, 8
-	octave 3
-	note F_, 8
-	note A_, 8
-	note B_, 4
-	octave 4
-	note C_, 4
-	notetype $6, $b3
-	octave 3
-	note A#, 8
-	note C#, 8
-	octave 2
-	note F#, 8
-	octave 1
-	note B_, 8
-	notetype $6, $b2
-	note __, 4
-	octave 3
-	note A#, 4
-	note A#, 8
-	note G#, 4
-	note G#, 8
-	note G_, 4
-	note G_, 8
-	note F#, 4
-	note F#, 8
-	octave 2
-	note A_, 4
-	note A_, 8
-	note G#, 4
-	note G#, 8
-	note G_, 4
-	note G_, 8
-	note F#, 4
-	note A_, 8
-	note A_, 4
-	note A_, 8
-	note G#, 6
-	note G#, 6
-	note F#, 4
-	note G_, 4
 	note F_, 4
-	notetype $8, $b3
-	octave 4
-	note F_, 1
-	note G_, 1
-	note A_, 1
-	note A#, 1
-	note B_, 1
-	octave 5
-	note C_, 1
-	dutycycle $2
-	notetype $c, $b3
+	note E_, 1
+	note D#, 1
+	note D_, 4
+	octave 3
+	note D#, 1
+	note E_, 1
+	note F_, 4
+	note E_, 1
+	note D#, 1
+	note D_, 6
+	note C_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note C_, 2
+	note D_, 16
+	endchannel
+
+Music_GoldSilverOpening_Ch2:
+; Pat 0 Row 0
+	dutycycle $0
+	notetype $4, $14
+	vibrato $8, $12
+	stereopanning $f
+	note __, 1
+	notetype $c, $14
+; Pat 0 Row 0
+; Pat 1 Row 0
+; Pat 2 Row 0
+	callchannel Music_GoldSilverOpening_P1
+; Pat 3 Row 0
+	note G_, 5
+	notetype $8, $a4
+	note __, 1
+	notetype $c, $a4
+	note D_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note G_, 2
+	note F_, 6
+	note D_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note F_, 2
+	note D#, 6
+	octave 2
+	note B_, 2
+	note G_, 2
+	octave 3
+	note D#, 2
+	note F_, 6
+	octave 2
+	note B_, 2
+	note G_, 2
+	octave 3
+	note F_, 2
+	note G_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note F_, 2
+	note G_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note F_, 2
+	note G_, 4
+	endchannel
+
+Music_GoldSilverOpening_Ch3:
+; Pat 0 Row 0
+	notetype $c, $33
+	stereopanning $ff
+	octave 2
+	note G_, 16
+	intensity $23
+	note G_, 14
+	notetype $4, $13
+	octave 3
 	note D_, 1
 	note C#, 1
-	note D_, 4
-	note A_, 1
-	note G#, 1
-	note A_, 4
-	octave 4
-	note A#, 2
-	octave 5
-	note D_, 2
-	note F_, 2
-	note G_, 6
-	note D#, 2
-	note D_, 2
-	note C_, 2
-	note D#, 4
-	note F#, 2
-	dutycycle $3
-	notetype $8, $92
-	note A_, 1
-	note F_, 1
-	note D#, 1
 	note C_, 1
-	octave 4
+	octave 2
+	note B_, 1
+	note A#, 1
 	note A_, 1
-	note G_, 1
-	note F_, 1
-	note D#, 1
-	note C_, 1
-	octave 3
-	note A_, 3
-	notetype $c, $b2
-	note A#, 2
-	note B_, 2
-	octave 4
-	note C_, 2
-	note D_, 2
-	note D#, 2
-	intensity $c2
-	note F_, 2
-	note G_, 2
-	note A_, 16
-	endchannel
-; f70f8
-
-Music_GoldSilverOpening_Ch2: ; f70f8
-	tone $0001
-	stereopanning $f
-	notetype $c, $b7
-	dutycycle $2
-	octave 2
-	note C_, 16
-	note C_, 16
-	dutycycle $3
-	notetype $c, $b2
-	note __, 2
-	notetype $6, $62
-	octave 1
-	note F_, 2
-	note A_, 2
-	octave 2
-	note C_, 2
-	note E_, 2
-	note F_, 2
-	note A_, 2
-	octave 3
-	note C_, 2
-	note E_, 2
-	note F_, 2
-	note A_, 2
-	octave 4
-	note C_, 2
-	note E_, 2
-	note F_, 2
-	note A_, 2
-	octave 5
-	note C_, 2
-	note E_, 2
-	octave 1
-	note D#, 2
-	note G_, 2
-	note A#, 2
-	octave 2
-	note D_, 2
-	note D#, 2
-	note G_, 2
-	note A#, 2
-	octave 3
-	note D_, 2
-	note D#, 2
-	note G_, 2
-	note A#, 2
-	octave 4
-	note D_, 2
-	note D#, 2
-	note G_, 2
-	notetype $c, $b7
-	octave 1
+	notetype $c, $13
+; Pat 1 Row 0
+	note G_, 16
 	note F_, 16
-	intensity $83
-	octave 4
-	note F_, 2
-	intensity $81
-	note D_, 2
-	intensity $93
-	note G#, 2
-	intensity $91
-	note F_, 2
-	intensity $a3
-	octave 5
-	note C#, 2
-	intensity $a1
-	octave 4
-	note A#, 2
-	intensity $b3
-	octave 5
-	note F#, 2
-	intensity $c1
-	note D#, 2
-	intensity $b3
-	octave 1
-	note A#, 2
-	notetype $6, $b2
-	octave 4
-	note D_, 4
-	note D_, 4
-	octave 1
-	note F_, 4
-	octave 4
-	note D_, 4
-	note D_, 4
-	octave 2
-	note C_, 4
-	octave 4
-	note D#, 4
-	note D#, 4
-	octave 1
-	note F_, 4
-	octave 4
-	note D#, 4
-	note D#, 4
-	octave 1
-	note A#, 4
-	octave 3
-	note D_, 4
-	note D_, 4
-	octave 1
-	note B_, 4
-	octave 3
-	note D_, 4
-	note D_, 4
-	octave 2
-	note C_, 4
-	octave 3
-	note D#, 4
-	note D#, 4
-	octave 1
-	note B_, 4
-	octave 3
-	note D#, 4
-	note F#, 4
-	octave 1
-	note A#, 4
-	octave 3
-	note D_, 4
-	note D_, 4
-	octave 1
-	note B_, 4
-	octave 3
-	note D_, 6
-	note D_, 2
-	octave 2
-	note C_, 4
-	octave 3
-	note D#, 4
 	note D#, 16
+	note F_, 16
+; Pat 2 Row 0
+Music_GoldSilverOpening_Ch3_L1:
+	intensity $13
+	octave 3
+	note G_, 1
+	intensity $33
 	octave 2
-	note D#, 2
+	note G_, 1
+	intensity $13
+	note G_, 1
+	octave 3
+	note G_, 1
+	octave 2
+	note G_, 1
+	intensity $33
+	note G_, 1
+	intensity $13
+	octave 3
+	note G_, 1
+	octave 2
+	intensity $33
+	note G_, 1
+	intensity $13
+	note G_, 1
+	octave 3
+	note G_, 1
+	octave 2
+	note G_, 1
+	intensity $33
+	note G_, 1
+	intensity $13
+	octave 3
+	note G_, 1
+	intensity $33
+	octave 2
+	note G_, 1
+	intensity $13
+	note G_, 1
+	octave 3
+	note G_, 1
+	octave 2
+	note G_, 1
+	intensity $33
+	note G_, 1
+	intensity $13
+	octave 3
+	note G_, 1
+	octave 2
+	intensity $33
+	note G_, 1
+	intensity $13
+	note G_, 1
+	octave 3
+	note G_, 1
+	note F#, 1
+	intensity $33
+	octave 2
+	note F#, 1
+	loopchannel 2, Music_GoldSilverOpening_Ch3_L1
+	intensity $13
+	octave 3
+	note G_, 1
+	octave 2
+	intensity $33
+	note G_, 1
+	intensity $13
+	note G_, 1
+	octave 3
+	note G_, 1
+	octave 2
+	note G_, 1
+	intensity $33
+	note G_, 1
+	intensity $13
+	octave 3
+	note G_, 1
+	octave 2
+	intensity $33
+	note G_, 1
+	intensity $13
+	note G_, 1
+	octave 3
+	note G_, 1
+	octave 2
+	note G_, 1
+	intensity $33
+	note G_, 1
+	intensity $13
+	note G_, 10
+	octave 3
+	note F_, 1
+	intensity $33
+	octave 2
+	note F_, 1
+; Pat 3 Row 0
+	intensity $13
+	octave 3
+	note G_, 1
+	intensity $33
+	octave 2
+	note G_, 1
+	intensity $13
+	note G_, 1
+	octave 3
+	note G_, 1
+	octave 2
+	note G_, 1
+	intensity $33
+	note G_, 1
+	intensity $13
+	octave 3
+	note G_, 1
+	octave 2
+	intensity $33
+	note G_, 1
+	intensity $13
+	note G_, 1
+	octave 3
+	note G_, 1
+	octave 2
+	note G_, 1
+	intensity $33
+	note G_, 1
+	intensity $13
+	octave 3
+	note F_, 1
+	octave 2
+	intensity $33
+	note F_, 1
+	intensity $13
+	note F_, 1
+	octave 3
+	note F_, 1
+	octave 2
+	note F_, 1
+	intensity $33
+	note F_, 1
+	intensity $13
+	octave 3
+	note F_, 1
+	octave 2
+	intensity $33
+	note F_, 1
+	intensity $13
+	note F_, 1
+	octave 3
+	note F_, 1
+	octave 2
+	note F_, 1
+	intensity $33
+	note F_, 1
+	intensity $13
+	octave 3
+	note D#, 1
+	octave 2
+	intensity $33
+	note D#, 1
+	intensity $13
+	note D#, 1
+	octave 3
+	note D#, 1
+	octave 2
+	note D#, 1
+	intensity $33
+	note D#, 1
+	intensity $13
+	octave 3
+	note D#, 1
+	octave 2
+	intensity $33
+	note D#, 1
+	intensity $13
+	note D#, 1
+	octave 3
+	note D#, 1
+	octave 2
+	note D#, 1
+	intensity $33
+	note D#, 1
+	intensity $13
+	octave 3
+	note F_, 1
+	octave 2
+	intensity $33
+	note F_, 1
+	intensity $13
+	note F_, 1
+	octave 3
+	note F_, 1
+	octave 2
+	note F_, 1
+	intensity $33
+	note F_, 1
+	intensity $13
+	octave 3
+	note F_, 1
+	octave 2
+	intensity $33
+	note F_, 1
+	intensity $13
+	note F_, 1
+	octave 3
+	note F_, 1
+	octave 2
+	note F_, 1
+	intensity $33
+	note F_, 1
+	intensity $13
+	octave 3
+	note G_, 1
+	octave 2
+	intensity $33
+	note G_, 1
+	intensity $13
+	note G_, 1
+	intensity $33
+	note G_, 1
+	intensity $13
+	note G_, 1
+	intensity $33
+	note G_, 1
+	intensity $13
+	octave 3
+	note G_, 1
+	octave 2
+	intensity $33
+	note G_, 1
+	intensity $13
+	note G_, 1
+	intensity $33
+	note G_, 1
+	intensity $13
+	note G_, 1
+	intensity $33
+	note G_, 1
+	intensity $13
 	octave 3
 	note G_, 4
-	note G_, 4
-	note G_, 2
-	octave 2
-	note D#, 4
-	octave 3
-	note F#, 4
-	note F#, 4
-	octave 2
-	note D_, 2
-	octave 3
-	note F_, 4
-	note F_, 4
-	note F_, 2
-	octave 2
-	note G_, 4
-	octave 3
-	note B_, 4
-	note B_, 4
-	octave 2
-	note C_, 4
-	octave 3
-	note D#, 4
-	note D#, 4
-	octave 2
-	note C_, 4
-	octave 3
-	note D#, 4
-	note D#, 16
-	notetype $6, $a2
-	note F_, 4
-	note G_, 4
-	note G#, 4
-	notetype $6, $c2
-	note A_, 4
-	note A#, 4
-	octave 4
-	note C_, 4
-	notetype $6, $d2
-	octave 3
-	note A_, 4
-	octave 4
-	note C_, 4
-	note D#, 16
 	endchannel
-; f71c8
 
-Music_GoldSilverOpening_Ch3: ; f71c8
-	notetype $6, $10
-	octave 2
-	note D_, 4
-	notetype $6, $20
-	note D_, 4
-	notetype $6, $30
-	note D_, 4
-	note __, 12
-	note __, 8
-	notetype $6, $10
-	octave 2
-	note D#, 1
-	note __, 1
+Music_GoldSilverOpening_P1:
+	octave 1
 	note G_, 1
-	note __, 1
-	note A#, 1
-	note __, 1
+	note B_, 1
+	octave 2
+	note D_, 1
+	intensity $24
+	note F#, 1
+	note G_, 1
+	note B_, 1
+	intensity $34
 	octave 3
 	note D_, 1
-	note __, 1
-	note D#, 1
-	note __, 1
+	note F#, 1
 	note G_, 1
-	note __, 1
-	note A#, 1
-	note __, 1
+	intensity $44
+	note B_, 1
 	octave 4
 	note D_, 1
-	note __, 1
-	note D#, 1
-	note __, 1
+	note F#, 1
+	intensity $54
 	note G_, 1
-	note __, 1
-	note A#, 1
-	note __, 1
+	note B_, 1
 	octave 5
 	note D_, 1
-	note __, 1
-	note D#, 1
-	note __, 1
+	note F#, 1
+	intensity $64
+	octave 1
 	note G_, 1
-	note __, 1
-	note A#, 1
-	note __, 1
-	octave 6
-	note D_, 1
-	note __, 1
+	note B_, 1
 	octave 2
+	note D_, 1
+	intensity $74
+	note F#, 1
+	note G_, 1
+	note B_, 1
+	intensity $84
+	octave 3
+	note D_, 1
+	note F#, 1
+	note G_, 1
+	intensity $94
+	note B_, 1
+	octave 4
+	note D_, 1
+	note F#, 1
+	intensity $a4
+	note G_, 1
+	note B_, 1
+	octave 5
+	note D_, 1
+	note F#, 1
+; Pat 1 Row 0
+	tempo $a8
+	octave 1
+	note G_, 1
+	note B_, 1
+	octave 2
+	note D_, 1
+	note F#, 1
+	note G_, 1
+	note B_, 1
+	octave 3
+	note D_, 1
+	note F#, 1
+	note G_, 1
+	note B_, 1
+	octave 4
+	note D_, 1
+	note F#, 1
+	note G_, 1
+	note B_, 1
+	octave 5
+	note D_, 1
+	note F#, 1
+	octave 1
 	note F_, 1
-	note __, 1
 	note A_, 1
-	note __, 1
+	octave 2
+	note C_, 1
+	note E_, 1
+	note F_, 1
+	note A_, 1
 	octave 3
 	note C_, 1
-	note __, 1
 	note E_, 1
-	note __, 1
 	note F_, 1
-	note __, 1
 	note A_, 1
-	note __, 1
 	octave 4
 	note C_, 1
-	note __, 1
 	note E_, 1
-	note __, 1
 	note F_, 1
-	note __, 1
 	note A_, 1
-	note __, 1
 	octave 5
 	note C_, 1
-	note __, 1
 	note E_, 1
-	note __, 1
-	note F_, 1
-	note __, 1
-	note A_, 1
-	note __, 1
-	octave 6
-	note C_, 1
-	note __, 1
-	note E_, 1
-	note __, 1
+	octave 1
+	note D#, 1
+	note G_, 1
+	note A#, 1
 	octave 2
+	note D_, 1
 	note D#, 1
-	note __, 1
 	note G_, 1
-	note __, 1
 	note A#, 1
-	note __, 1
 	octave 3
 	note D_, 1
-	note __, 1
 	note D#, 1
-	note __, 1
 	note G_, 1
-	note __, 1
 	note A#, 1
-	note __, 1
 	octave 4
 	note D_, 1
-	note __, 1
 	note D#, 1
-	note __, 1
 	note G_, 1
-	note __, 1
 	note A#, 1
-	note __, 1
 	octave 5
 	note D_, 1
-	note __, 1
-	note D#, 1
-	note __, 1
-	note G_, 1
-	note __, 1
-	note A#, 1
-	note __, 1
-	octave 6
-	note D_, 1
-	note __, 1
-	notetype $8, $10
-	note F_, 1
-	octave 5
-	note B_, 1
-	note A_, 1
-	note B_, 1
-	note A_, 1
+	octave 1
 	note F_, 1
 	note A_, 1
-	note F_, 1
-	octave 4
-	note B_, 1
-	octave 5
-	note F_, 1
-	octave 4
-	note B_, 1
-	note A_, 1
-	note B_, 1
-	note A_, 1
-	note F_, 1
-	note A_, 1
-	note F_, 1
-	octave 3
-	note B_, 1
-	octave 4
-	note F_, 1
-	octave 3
-	note B_, 1
-	note A_, 1
-	note B_, 1
-	note A_, 1
-	note F_, 1
-	octave 5
-	note A_, 3
-	notetype $c, $10
-	note G_, 1
-	note __, 1
-	octave 6
-	note C_, 2
-	octave 5
-	note A#, 1
-	note __, 1
-	octave 6
-	note F_, 2
-	note D#, 1
-	note __, 1
-	note A#, 2
-	note G#, 1
-	note __, 1
-	note __, 16
-	note __, 4
-	notetype $6, $10
 	octave 2
-	note G_, 1
-	note __, 3
-	note A_, 2
-	note __, 2
+	note C_, 1
+	note E_, 1
+	note F_, 1
+	note A_, 1
+	octave 3
+	note C_, 1
+	note E_, 1
+	note F_, 1
+	note A_, 1
+	octave 4
+	note C_, 1
+	note E_, 1
+	note F_, 1
+	note A_, 1
 	octave 5
-	note F_, 12
-	note D_, 4
-	note D_, 6
-	note F_, 2
-	note D#, 12
-	note G#, 2
-	note __, 2
-	note F#, 6
-	note D#, 2
-	note F_, 12
+	note C_, 1
+	note E_, 1
+; Pat 2 Row 0
+	note G_, 2
+	octave 4
+	intensity $54
 	note D_, 2
-	note __, 2
-	note D#, 6
-	note F_, 2
-	note G_, 2
-	note __, 2
-	note F#, 2
-	note __, 2
-	note G_, 2
-	note __, 2
-	note A_, 4
-	note D#, 4
-	note F_, 2
-	note __, 2
-	octave 4
-	note A#, 1
-	note __, 1
-	octave 5
-	note D_, 1
-	note __, 1
-	octave 4
-	note A#, 1
-	note __, 1
-	octave 5
-	note D_, 1
-	note __, 1
-	octave 4
-	note A#, 1
-	note __, 1
-	octave 5
-	note D_, 1
-	note __, 1
-	octave 4
-	note A#, 1
-	note __, 1
-	octave 5
-	note C_, 1
-	note __, 1
-	octave 4
-	note A#, 1
-	note __, 1
-	octave 5
-	note C_, 1
-	note __, 1
-	octave 4
-	note A#, 1
-	note __, 1
-	octave 5
-	note C_, 1
-	note __, 1
-	octave 4
-	note A#, 1
-	note __, 1
-	octave 5
-	note D_, 1
-	note __, 1
-	octave 4
-	note A#, 1
-	note __, 1
-	octave 5
-	note D_, 1
-	note __, 1
-	octave 4
-	note A#, 1
-	note __, 1
-	octave 5
-	note D_, 1
-	note __, 1
-	octave 4
-	note A#, 1
-	note __, 1
-	octave 5
-	note C_, 1
-	note __, 1
-	note D_, 1
-	note __, 1
-	note F_, 1
-	note __, 1
-	note D_, 1
-	note __, 1
-	note F_, 1
-	note __, 1
-	note G_, 1
-	note __, 1
-	note A#, 1
-	note __, 1
-	note G_, 1
-	note __, 1
-	note A#, 1
-	note __, 1
-	note G_, 1
-	note __, 1
-	note A#, 1
-	note __, 1
-	note F#, 1
-	note __, 1
-	note A#, 1
-	note __, 1
-	note F#, 1
-	note __, 1
-	note A#, 1
-	note __, 1
-	note F#, 1
-	note __, 1
-	note A#, 1
-	note __, 9
-	notetype $c, $10
-	note __, 14
+	note D_, 2
 	octave 3
-	note F_, 2
-	note D#, 1
-	note __, 1
+	note G_, 2
+	octave 4
+	note D_, 2
+	note D_, 2
+	octave 3
+	note G_, 2
+	octave 4
+	note D_, 2
+	note D_, 2
+	octave 3
+	note G_, 2
+	octave 4
+	note D_, 2
+	intensity $a4
+	octave 3
+	note F#, 2
+	note G_, 2
+	octave 4
+	intensity $54
+	note D_, 2
+	note D_, 2
+	octave 3
+	note G_, 2
+	octave 4
+	note D_, 2
+	note D_, 2
+	octave 3
+	note G_, 2
+	octave 4
+	note D_, 2
+	note D_, 2
+	octave 3
+	note G_, 2
+	octave 4
+	note D_, 2
+	intensity $a4
+	octave 3
+	note F#, 2
+	note G_, 2
+	octave 4
+	intensity $54
+	note D_, 2
+	note D_, 2
+	octave 3
+	note G_, 2
+	octave 4
+	note D_, 2
+	note D_, 2
+	intensity $a4
+	octave 1
+	note B_, 1
+	octave 2
 	note C_, 1
-	note __, 13
+	note D_, 1
+	note E_, 1
+	note F#, 1
+	note G_, 1
+	note A_, 1
+	note B_, 1
+	octave 3
+	note C_, 1
+	note D_, 1
+	note E_, 1
+	note F#, 1
 	endchannel
-; f7308
-
-
