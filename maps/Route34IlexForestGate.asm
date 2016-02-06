07a2763550f97655bd293f60a9c79866da3b3aa2
PikalaxALT <PikalaxALT@gmail.com>
2016-02-06 15:28:48 -0500

    Fix text error in route 34 gate

--- maps/Route34IlexForestGate.asm
+++ maps/Route34IlexForestGate.asm
@@ -100,8 +100,9 @@ UnknownText_0x62d9d: ; 0x62d9d
 	line "making a #DEX?"
 
 	para "It must be hard if"
-	line "#MON don't love you"
-	cont "Try using this TM."
+	line "#MON don't love"
+	cont "you."
+	para "Try using this TM."
 	
 	done
 ; 0x62df6
