2b0c5e9cd72159e759e90df47942496b2e9fe182
Beesafree <Beesafree@hotmail.com>
2016-02-11 15:09:33 -0500

    Update AI.py

--- ai/AI.py
+++ ai/AI.py
@@ -1035,7 +1035,9 @@ class AI(object):
                 mondata1['lockon'] = False
                 tempx = self._statsmultipliers[str(mondata1[mycurrent]['boosts']['spd'])]/100
                 tempy = self._statsmultipliers[str(mondata1[traincurrent]['boosts']['spd'])]/100
-
+                print(mycurrent)
+                print(traincurrent)
+                print(int(tempcombo[x1]))
                 if (mondata1[mycurrent]['stats']['speed'] * tempx > mondata1[traincurrent]['stats']['speed'] * tempy) or (mondata1[mycurrent]['moves'][int(tempcombo[x1])]['effect'] in ('extremespeed', 'priorityhit', 'endure', 'protect')):
 
                     if myhp > 0:
