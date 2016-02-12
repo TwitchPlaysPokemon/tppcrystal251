8282736bb386ae06f0f42e273a79f5249fc9e15b
Lightning <lightningswhq@gmail.com>
2016-02-11 21:39:44 -0500

    Mt Silver Basement

---
+++ maps/MtSilverRuinsB1.asm
@@ -0,0 +1,28 @@
+MtSilverRuinsB1_MapScriptHeader:
+	; triggers
+	db 0
+
+	; callbacks
+	db 0
+
+; <scripts go here>
+
+
+; <text goes here>
+
+
+MtSilverRuinsB1_MapEventHeader:
+	; filler
+	db 0, 0
+
+	; warps
+	db 0
+	
+	; coord events
+	db 0
+
+	; bg events
+	db 0
+
+	; object events
+	db 0
