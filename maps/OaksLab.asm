8a4c95d97990de190f0952198d04be23fcd9cdc7
Chaos-Lord <dd4791.mariokarter@hotmail.co.uk>
2016-02-12 13:24:54 +0000

    morty fixes

--- maps/OaksLab.asm
+++ maps/OaksLab.asm
@@ -230,6 +230,7 @@ UnknownScript_0x19b3f7: ; 0x19b3f7
 	writetext UnknownText_0x19b4fc
 	buttonsound
 	setevent EVENT_SET_BY_OAK_AFTER_16_BADGES
+	clearevent EVENT_MORTY_IS_AT_BACK_OF_GYM
 	jump UnknownScript_0x19b3ea
 ; 0x19b401
 
