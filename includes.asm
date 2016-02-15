93829f7c0ff5ed0a77465b6e06f5a1209fbae44c
Pigu-A <supa7764@gmail.com>
2016-02-15 11:43:33 +0700

    Version 0.9.1

--- includes.asm
+++ includes.asm
@@ -1,7 +1,7 @@
 ; BEESAFREE EQU 1
 ; MUSICPLYR EQU 1
 
-; please use 0.1 until we finish everything
+; please use 0.9 until we finish everything
 _VERSION EQUS "\"0.9.1\""
 _VERLEN  EQU  5 ; the length of version text
 
