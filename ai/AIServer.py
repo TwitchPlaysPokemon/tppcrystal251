d655d1677d0bb53dc76da4008b56d7e3ed1f22a1
Felk <studio.felk@googlemail.com>
2016-02-18 00:50:01 +0100

    improved AIServer error handling

--- ai/AIServer.py
+++ ai/AIServer.py
@@ -7,6 +7,7 @@ import json
 from flask import Flask, request
 
 import AI
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 PORT = 5001
@@ -20,35 +21,44 @@ logger = logging.getLogger("AIServer")
 
 # global ai result variable.
 # set by worker thread "calculate_next_move",
-# read by ai_retrieve requests
-ai_result = None
+# read by ai_retrieve requests.
+# initialize to something that is a valid backup input in most cases,
+# so eventual ai-retrieve-deadlocks can be resolved with a restart of this server. 
+ai_result = "move1"
 # global handle to actual AI
 Artificial = AI.AI()
 LastActions = []
 
+def get_backup_move(battle_state):
+    try:
+        if battle_state["battleState"]["requested action"] & 0x40:
+            try:
+                return "switch%d" % random.randint(1, len(battle_state["battleState"]["enemyParty"]["party"]))
+            except KeyError:
+                return "switch%d" % random.randint(1, 6)
+        else:
+            try:
+                return "move%d" % random.randint(1, len(battle_state["battleState"]["enemypokemon"]["moves"]))
+            except KeyError:
+                return "move%d" % random.randint(1, 4)
+    except KeyError:
+        # the whole battle_state is fucked
+        return "move%d" % random.randint(1, 4)
+
 def calculate_next_move(battle_state):
     global ai_result, Artificial, LastActions
-    if not (battle_state["battleState"]["requested action"] & 0x04):
-        LastActions = []
     # invoke AI.
     # catch exceptions and provide a fallback to not get the whole game stuck waiting on a move
     try:
+        if not (battle_state["battleState"]["requested action"] & 0x04):
+            LastActions = []
         battle_state["battleState"]["history"] = LastActions
         next_move = Artificial.MainBattle(battle_state)
     except Exception as e:
         logger.exception("The AI threw an exception with the following input: %s" % battle_state)
         # uh-oh! better fall back to "default ai"
-        if battle_state["battleState"]["requested action"] & 0x40:
-            try:
-                next_move = "switch{}".format(random.choice(range(len(battle_state["battleState"]["enemyParty"]["party"]))) + 1)
-            except KeyError:
-                next_move = "switch{}".format(random.choice(range(6)) + 1)
-        else:
-            try:
-                next_move = "move{}".format(random.choice(range(len(battle_state["battleState"]["enemypokemon"]["moves"]))) + 1)
-            except:
-                next_move = "move{}".format(random.choice(range(4)) + 1)
-    logger.info("calculated AI: %s" % next_move)
+        next_move = get_backup_move(battle_state)
+    logger.info("next move: %s" % next_move)
     LastActions.append(next_move)
     
     # set global ai result variable. do this always last to avoid race-conditions.
@@ -61,7 +71,11 @@ def ai_invoke():
     # force the request body to be parsed as JSON
     # because the lua-side doesn't specify the content-type
     # (it's easier on the lua-side this way)
-    battle_state = request.get_json(force=True)
+    try:
+        battle_state = request.get_json(force=True)
+    except BadRequest:
+        logger.exception("ai_invoke got called with nothing, or something not json-decodable: %s" % request)
+        battle_state = {}
     logger.info("invoking AI... submitted JSON: %s" % battle_state)
 
     if type(battle_state) == str:
@@ -82,6 +96,7 @@ def ai_retrieve():
     return ai_result if ai_result else ""
 
 def main():
+    app.debug = True
     app.run(port=PORT)
 
 if __name__ == '__main__':
