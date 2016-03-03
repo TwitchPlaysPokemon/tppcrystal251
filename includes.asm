f4b8e044ddc2c219717f1582cd393c4d57151a07
Lightning <lightningswhq@gmail.com>
2016-03-03 02:22:35 -0500

    Revert "0.9.19 : bump to 0.9.2 once trainers implemented in gym"
    This reverts commit 00334b0441dbfe9e22fcf8b8913073fff2a5007a.

--- includes.asm
+++ includes.asm
@@ -2,7 +2,7 @@
 ; MUSICPLYR EQU 1
 
 ; please use 0.9 until we finish everything
-_VERSION EQUS "\"0.9.19\""
+_VERSION EQUS "\"0.9.15\""
 _VERLEN  EQU  6 ; the length of version text
 
 INCLUDE "version.asm"
