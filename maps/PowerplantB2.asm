433a31d413e98c6b689cff8d41bfa90512e122f8
Lightning <lightningswhq@gmail.com>
2016-04-01 00:07:42 -0400

    Powerplant B2 Puzzle! :pogchamp:

--- maps/PowerplantB2.asm
+++ maps/PowerplantB2.asm
@@ -11,13 +11,23 @@ PowerplantB2_MapScriptHeader:
 
 ;puzzle order is RIGHT, LEFT, TOP, BOTTOM
 
-PowerplantB2SetBlocksScript:
+PowerplantB2SetBlocksScript
 	checkevent EVENT_ZAPDOS_DOOR_UNLOCKED
 	iffalse PowerplantB2SetBlocksFalseScript
 	;change all blocks to be on if we already did the puzzle
 	;switches
+	;right
 	changeblock 36, 14, $56
 	changeblock 38, 14, $55
+	;top
+	changeblock 18, 0, $57
+	changeblock 20, 0, $56
+	;left
+	changeblock 0, 14, $57
+	changeblock 2, 14, $56
+	;bottom
+	changeblock 20, 32, $56
+	changeblock 22, 32, $55
 	;center area
 	;right side
 	changeblock 24, 16, $56
@@ -40,7 +50,43 @@ PowerplantB2SetBlocksScript:
 	changeblock 22, 24, $56
 	;gate
 	changeblock 18, 24, $4d
-PowerplantB2SetBlocksFalseScript:
+	return
+	
+PowerplantB2SetBlocksFalseScript
+;only change blocks based on if we have generators active, only need to check in order and check 3 as 4 sets other flag
+	checkevent EVENT_ZAPDOS_GENERATOR_1_ON
+	iftrue PowerPlantB2Generator1BlockScript
+	return
+	
+PowerPlantB2Generator1BlockScript
+	changeblock 36, 14, $56
+	changeblock 38, 14, $55
+	changeblock 24, 16, $56
+	changeblock 24, 18, $56
+	changeblock 24, 20, $56
+	changeblock 24, 22, $56
+	checkevent EVENT_ZAPDOS_GENERATOR_2_ON
+	iftrue PowerPlantB2Generator2BlockScript
+	return
+
+PowerPlantB2Generator2BlockScript
+	changeblock 0, 14, $57
+	changeblock 2, 14, $56
+	changeblock 14, 16, $56
+	changeblock 14, 18, $56
+	changeblock 14, 20, $56
+	changeblock 14, 22, $56
+	checkevent EVENT_ZAPDOS_GENERATOR_3_ON
+	iftrue PowerPlantB2Generator3BlockScript
+	return
+
+PowerPlantB2Generator3BlockScript
+	changeblock 18, 0, $57
+	changeblock 20, 0, $56
+	changeblock 16, 14, $56
+	changeblock 18, 14, $56
+	changeblock 20, 14, $56
+	changeblock 22, 14, $56
 	return
 
 ZapdosScript1:
@@ -68,19 +114,30 @@ ZapdosText1:
 	
 GeneratorTextOnline:
 	text "The generator"
-	line "roared to life."
+	line "hums loudly."
+	
+	para "Better not touch"
+	line "anything<...>"
 	done
 	
 GeneratorTextSuccess:
-	text "STARUP SEQUENCE:"
+	text "The generator"
+	line "roared to life."
+	
+	para "STARUP SEQUENCE:"
 	line "CONTINUING"
 	done
 	
 GeneratorTextFail:
-	text "STARUP SEQUENCE:"
-	line "-FAILURE-"
+	text "The generator"
+	line "failed to start."
+	
+	para "STARUP SEQUENCE:"
+	line "FAILURE"
 	
 	para "POWER ORDER BROKEN"
+	
+	para "ALL GENERATORS"
 	line "SHUTTING DOWN<...>"
 	done
 	
@@ -91,9 +148,14 @@ GeneratorTextCompleted:
 	para "ACCESS GRANTED."
 	done
 	
-;generator 1 right
+GeneratorOnScript:
+	jumptext GeneratorTextOnline
+	end
+	
+;generator right
 ZapdosCaveGenerator1Script:
-
+	checkevent EVENT_ZAPDOS_GENERATOR_1_ON
+	iftrue GeneratorOnScript
 	waitsfx
 	playsound SFX_PUSH_BUTTON
 	waitsfx
@@ -112,16 +174,14 @@ ZapdosCaveGenerator1Script:
 	changeblock 24, 22, $56
 	reloadmappart
 	waitsfx
-	jumptext GeneratorTextOnline
-	;check to see if proper pattern here
-	;if wrong play sfx, change blocks off
-	;if true, keep blocks set, show text
+	setevent EVENT_ZAPDOS_GENERATOR_1_ON
 	jumptext GeneratorTextSuccess
 	end
 
-;generator 3 top
-ZapdosCaveGenerator2Script:
-
+;generator top
+ZapdosCaveGenerator3Script:
+	checkevent EVENT_ZAPDOS_GENERATOR_3_ON
+	iftrue GeneratorOnScript
 	waitsfx
 	playsound SFX_PUSH_BUTTON
 	waitsfx
@@ -140,16 +200,16 @@ ZapdosCaveGenerator2Script:
 	changeblock 22, 14, $56
 	reloadmappart
 	waitsfx
-	jumptext GeneratorTextOnline
-	;check to see if proper pattern here
-	;if wrong play sfx, change blocks off
-	;if true, keep blocks set, show text
+	checkevent EVENT_ZAPDOS_GENERATOR_2_ON
+	iffalse ResetAllGeneratorsScript
+	setevent EVENT_ZAPDOS_GENERATOR_3_ON
 	jumptext GeneratorTextSuccess
 	end
 
-;generator 2 left
-ZapdosCaveGenerator3Script:
-
+;generator left
+ZapdosCaveGenerator2Script:
+	checkevent EVENT_ZAPDOS_GENERATOR_2_ON
+	iftrue GeneratorOnScript
 	waitsfx
 	playsound SFX_PUSH_BUTTON
 	waitsfx
@@ -168,16 +228,16 @@ ZapdosCaveGenerator3Script:
 	changeblock 14, 22, $56
 	reloadmappart
 	waitsfx
-	jumptext GeneratorTextOnline
-	;check to see if proper pattern here
-	;if wrong play sfx, change blocks off
-	;if true, keep blocks set, show text
+	checkevent EVENT_ZAPDOS_GENERATOR_1_ON
+	iffalse ResetAllGeneratorsScript
+	setevent EVENT_ZAPDOS_GENERATOR_2_ON
 	jumptext GeneratorTextSuccess
 	end
-
-;generator 4 bottom
+	
+;generator bottom
 ZapdosCaveGenerator4Script:
-
+	checkevent EVENT_ZAPDOS_GENERATOR_4_ON
+	iftrue GeneratorOnScript
 	waitsfx
 	playsound SFX_PUSH_BUTTON
 	waitsfx
@@ -195,19 +255,72 @@ ZapdosCaveGenerator4Script:
 	changeblock 22, 24, $56
 	reloadmappart
 	waitsfx
-	jumptext GeneratorTextOnline
-	;check to see if proper pattern here
-	;if wrong play sfx, change blocks off
-	;if true, keep blocks set, show text, set flag and open gate
-	jumptext GeneratorTextCompleted
-	;gate
-	earthquake 90
+	checkevent EVENT_ZAPDOS_GENERATOR_3_ON
+	iffalse ResetAllGeneratorsScript
+	setevent EVENT_ZAPDOS_GENERATOR_4_ON
+	setevent EVENT_ZAPDOS_DOOR_UNLOCKED
+	loadfont
+	writetext GeneratorTextSuccess
+	waitbutton
+	closetext
 	playsound SFX_STRENGTH
+	earthquake 90
+	waitsfx
 	refreshscreen 0
 	changeblock 18, 24, $4d
 	reloadmappart
+	jumptext GeneratorTextCompleted
 	end
 
+ResetAllGeneratorsScript:
+	playsound SFX_COMET_PUNCH
+	earthquake 90
+	waitsfx
+	refreshscreen 0
+	;change all blocks
+	;switches
+	;right
+	changeblock 36, 14, $4b
+	changeblock 38, 14, $4a
+	;top
+	changeblock 18, 0, $4c
+	changeblock 20, 0, $4b
+	;left
+	changeblock 0, 14, $4c
+	changeblock 2, 14, $4b
+	;bottom
+	changeblock 20, 32, $4b
+	changeblock 22, 32, $4a
+	;center area
+	;right side
+	changeblock 24, 16, $4b
+	changeblock 24, 18, $4b
+	changeblock 24, 20, $4b
+	changeblock 24, 22, $4b
+	;left side
+	changeblock 14, 16, $4b
+	changeblock 14, 18, $4b
+	changeblock 14, 20, $4b
+	changeblock 14, 22, $4b
+	;top
+	changeblock 16, 14, $4b
+	changeblock 18, 14, $4b
+	changeblock 20, 14, $4b
+	changeblock 22, 14, $4b
+	;bottom
+	changeblock 16, 24, $4b
+	changeblock 20, 24, $4b
+	changeblock 22, 24, $4b
+	;gate
+	changeblock 18, 24, $48
+	reloadmappart
+	clearevent EVENT_ZAPDOS_GENERATOR_1_ON
+	clearevent EVENT_ZAPDOS_GENERATOR_2_ON
+	clearevent EVENT_ZAPDOS_GENERATOR_3_ON
+	clearevent EVENT_ZAPDOS_GENERATOR_4_ON
+	clearevent EVENT_ZAPDOS_DOOR_UNLOCKED
+	jumptext GeneratorTextFail
+	end
 
 PowerplantB2_MapEventHeader:
 	; filler
@@ -224,8 +337,8 @@ PowerplantB2_MapEventHeader:
 	; bg events
 	db 4
 	signpost 15, 39, $0, ZapdosCaveGenerator1Script
-	signpost 1, 18, $0, ZapdosCaveGenerator2Script
-	signpost 15, 0, $0, ZapdosCaveGenerator3Script
+	signpost 15, 0, $0, ZapdosCaveGenerator2Script
+	signpost 1, 18, $0, ZapdosCaveGenerator3Script
 	signpost 33, 23, $0, ZapdosCaveGenerator4Script
 	
 	; object events
