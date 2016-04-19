aca41f87bfc2b8540d5454ebac8df16377d7f6d0
Pigu-A <supa7764@gmail.com>
2016-04-19 13:46:25 +0700

    Fahrenheit :dansgame:

--- data/pokedex/entries/136.asm
+++ data/pokedex/entries/136.asm
@@ -2,6 +2,7 @@
 	db "FLAME@" ; species name
 	dw 211, 550 ; height, width
 
+	metricdex .metric
 	db   "Once it has stored"
 	next "up enough heat,"
 	next "this #MON's"
@@ -10,3 +11,10 @@
 	next "1700 degrees.@"
 ; 0x1cc353
 
+.metric
+	db   "Once it has stored"
+	next "up enough heat,"
+	next "this #MON's"
+	page "body temperature"
+	next "can reach up to"
+	next "900 degrees.@"
