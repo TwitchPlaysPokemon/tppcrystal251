e24272e29eb0b869b966d5c1f56fb17f1a8677ca
pikalaxalt <PikalaxALT@gmail.com>
2016-04-26 12:30:21 -0400

    Add a script to automatically build the files for release on the github.io page

---
+++ build_release.sh
@@ -0,0 +1,42 @@
+filesdir=/cygdrive/d/tppcrystal251/files
+vbasrcdir=/cygdrive/c/Users/Scott/Downloads/vba-rerecording-svn480-win32
+version=1.1.1
+tmpdir=/tmp/crystal251
+
+# Do not modify anything below this line.
+# ---------------------------------------
+
+# Reset temp directory
+if [ -d $tmpdir ]
+then
+	rm -rf $tmpdir/*
+else
+	mkdir $tmpdir
+fi
+mkdir $tmpdir/AI
+mkdir $tmpdir/VBA-RR-243
+mkdir $tmpdir/VBA-RR-243/lua
+
+# Build the IPS files
+make patches
+
+# Copy the standalone patch to the webpage files directory
+cp pokecrystal11.ips $filesdir/annicrystal_$version.ips
+
+# Copy the AI patch to the temporary directory
+cp pokecrystal_ai.ips $tmpdir/annicrystal_aI_$version.ips
+
+# Copy the AI scripts and data to the temp AI subdirectory
+cp ai/AI.py ai/AIServer.py ai/AIServerStandalone.py ai/ai_data.py ai/AiMoves.txt $tmpdir/AI/
+
+# Copy the emulator to the correct filename in the temp VBA-RR-243 subdirectory
+cp $vbasrcdir/VBA_VS2015_V72XR.exe $tmpdir/VBA-RR-243/VBA-RR-243.exe
+
+# Copy the lua DLLs to the temp VBA-RR-243 subdirectory
+cp $vbasrcdir/lua5*1.dll $tmpdir/VBA-RR-243/
+
+# Copy the lua scripts and dependencies to the temp VBA-RR-243/lua subdirectory
+cp -r lua_scripts/mime lua_scripts/socket lua_scripts/battle_ram.lua lua_scripts/constants.lua lua_scripts/JSON.lua lua_scripts/ltn12.lua lua_scripts/mime.lua lua_scripts/pokecrystal_ai.lua lua_scripts/readstates.lua lua_scripts/socket.lua $tmpdir/VBA-RR-243/lua/
+
+# Build the ZIP file to the webpage files directory (quiet mode, recursive)
+zip -r -q $filesdir/annicrystal_ai.zip $tmpdir/AI $tmpdir/VBA-RR-243 $tmpdir/annicrystal_aI_$version.ips
