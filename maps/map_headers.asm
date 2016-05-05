bb0acfae699d494cebd7ead645beb6bc76b895d0
pikalaxalt <PikalaxALT@gmail.com>
2016-05-05 00:22:50 -0400

    Some fixes to Cinnabar Volcano Lab event

--- maps/map_headers.asm
+++ maps/map_headers.asm
@@ -508,7 +508,7 @@ MapGroup27:
 	map_header PowerplantB1, 40, 7, POWER_PLANT, MUSIC_SILPH_CO, 1, 1, 1
 	map_header PowerplantB2, 43, 7, POWER_PLANT, MUSIC_SILPH_CO, 1, 2, 1
 	map_header EnteiRoom, 30, 7, CINNABAR_VOLCANO, MUSIC_MT_MOON, 1, 1, 1
-	map_header RaikouRoom, $c, 3, RADIO_TOWER, 1<<7 | MUSIC_GOLDENROD_CITY, 1, 1, 1
+	map_header RaikouRoom, $c, 3, RADIO_TOWER, 1 << 7 | MUSIC_GOLDENROD_CITY, 1, 1, 1
 	map_header SuicuneRoom, 30, 7, TOHJO_FALLS, MUSIC_UNION_CAVE, 1, 2, 1
 
 MapGroup28:
