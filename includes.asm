a8268d236d25596e44f4ff3256d9f8a2c0708fa1
pikalaxalt <PikalaxALT@gmail.com>
2016-05-14 19:54:29 -0400

    Version 1.2.1

--- includes.asm
+++ includes.asm
@@ -1,7 +1,7 @@
 ; BEESAFREE EQU 1
 
-_VERSION EQUS "\"1.2\""
-_VERLEN  EQU  3 ; the length of version text
+_VERSION EQUS "\"1.2.1\""
+_VERLEN  EQU  5 ; the length of version text
 
 INCLUDE "version.asm"
 INCLUDE "contents.asm"
